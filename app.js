4c676ffddfc39ee8462a61396c55e918595d7733
roryjackd <roryjackd@yahoo.com>
2018-07-22 13:39:18 -0600

    Campground model added

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose")
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    Campground = require("./models/campground")
 
 mongoose.connect("mongodb://localhost/app_camp")
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 // SCHEMA SETUP
-var campgroundSchema = mongoose.Schema({
-  name: String,
-  image: String,
-  description: String
-});
 
-var Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create(
 //   { 
---
+++ models/campground.js
@@ -0,0 +1,10 @@
+var mongoose = require("mongoose");
+
+
+var campgroundSchema = mongoose.Schema({
+  name: String,
+  image: String,
+  description: String
+});
+
+module.exports = mongoose.model("Campground", campgroundSchema);
