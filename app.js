ea72d7bed0cd09449b715272bbcc48343205981a
roryjackd <roryjackd@yahoo.com>
2018-07-29 15:07:06 -0600

    heroku mongodb added

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")    
 
-mongoose.connect("mongodb://localhost/yelp_camp_v4");
+//mongoose.connect("mongodb://localhost/yelp_camp_v4");
+mongoose.connect("mongodb://roryjackd:a1154723@ds227525.mlab.com:27525/appcamp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
