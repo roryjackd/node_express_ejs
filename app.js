b6672746ef0478219c420021fc328b170d7f9196
roryjackd <roryjackd@yahoo.com>
2018-07-29 15:36:35 -0600

    heroku port change

--- app.js
+++ app.js
@@ -49,8 +49,13 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, () => console.log("The Server has Started"))
-
+//local
+//app.listen(3000, () => console.log("The Server has Started"))
+//heroku
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+ console.log('App Server has started');
+});
 
 
 
