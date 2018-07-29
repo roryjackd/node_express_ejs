76197fb1c8f204df6e08e379f3d87c38a9004de5
roryjackd <roryjackd@yahoo.com>
2018-07-29 15:20:21 -0600

    heroku changes seeds

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
-seedDB();
+//seedDB();
 
 // PASSPORT CONFIGURATION
 app.use(require("express-session")({
